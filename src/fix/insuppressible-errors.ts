a72199a5412147150c60ad1f526f8cb6cfb951a0
Mark McDonald <mark.mcdonald@greenhouse.io>
2022-08-15 11:10:54 -0400

    Mark non-portable inferred types as insuppressible

--- src/fix/insuppressible-errors.ts
+++ src/fix/insuppressible-errors.ts
@@ -7,7 +7,24 @@ import { Diagnostic, ts } from "ts-morph";
  * Feel free to add additional error codes here as you find them.
  */
 export const insuppressibleErrors = new Set([
-  1383, 2304, 2306, 2503, 2578, 4025, 6059, 6307,
+  // Only named exports may use 'export type'.
+  1383,
+  // Cannot find name '{0}'.
+  2304,
+  // File '{0}' is not a module.
+  2306,
+  // Cannot find namespace '{0}'.
+  2503,
+  // Unused '@ts-expect-error' directive.
+  2578,
+  // The inferred type of '{0}' cannot be named without a reference to '{1}'. This is likely not portable. A type annotation is necessary.
+  2742,
+  // Exported variable '{0}' has or is using private name '{1}'.
+  4025,
+  // File '{0}' is not under 'rootDir' '{1}'. 'rootDir' is expected to contain all source files.
+  6059,
+  // File '{0}' is not listed within the file list of project '{1}'. Projects must list all files or use an 'include' pattern.
+  6307,
 ]);
 
 export function isDiagnosticSuppressible(
